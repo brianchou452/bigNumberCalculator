b68bbb94107af5d7ea1aea14e93d4a400bd6c24d
水獺 <brianchou452@gmail.com>
2022-04-08 16:21:47 +0800

    Initial Commit

---
+++ bigNumberCalculator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  bigNumberCalculator
+//
+//  Created by Brian Chou on 2022/4/8.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
